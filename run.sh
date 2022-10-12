7ea28e8ee6998e0b95c5399231d81ca6b5f34362
zCubed3 <liamr2023@gmail.com>
2022-10-11 23:23:12 -0700

    Fix things

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ $(docker images -q zcubed/openai-bot:latest 2> /dev/null) = "true" ]; then
+if [ $(docker images -q zcubed/openai-bot:latest 2> /dev/null) = "" ]; then
     docker zcubed/openai-bot:latest
 fi
 
-if [ $(docker inspect -f '{{.State.Running}}' omnibot-instance) = "true" ]; then
+if [ $(docker inspect -f '{{.State.Running}}' openai-bot:latest) = "true" ]; then
     docker stop openai-bot
     docker container rm openai-bot
 fi
 
 git pull
-docker build --tag zcubed/omnibot .
+docker build --tag zcubed/openai-bot .
 docker run --restart=always --network=host -dit -v "$(pwd):/bot" --name "openai-bot" zcubed/openai-bot:latest
